48f4b834b30a34d311a16214cdcd9b7aede82545
SkuldNorniern <skuldnorniern@gmail.com>
2024-11-01 11:34:35 +0900

    feat: add added functions to `lib.rs`

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,74 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::fmt::{Display, Formatter};
+
+pub mod loss;
+pub mod nn;
+pub mod prelude;
+pub mod serialize;
+pub mod tensor;
+
+
+use tensor::TensorError;
+use loss::LossError;
+
+#[derive(Debug)]
+pub enum MlError {
+    TensorError(TensorError),
+    LossError(LossError),
+    StringError(String),
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Display for MlError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            MlError::TensorError(e) => write!(f, "Tensor error: {}", e),
+            MlError::LossError(e) => write!(f, "Loss error: {}", e),
+            MlError::StringError(s) => write!(f, "{}", s),
+        }
     }
 }
+
+impl std::error::Error for MlError {}
+
+impl From<TensorError> for MlError {
+    fn from(error: TensorError) -> Self {
+        MlError::TensorError(error)
+    }
+}
+
+impl From<LossError> for MlError {
+    fn from(error: LossError) -> Self {
+        MlError::LossError(error)
+    }
+}
+
+impl From<MlError> for TensorError {
+    fn from(val: MlError) -> Self {
+        match val {
+            MlError::TensorError(e) => e,
+            _ => unreachable!(),
+        }
+    }
+}
+
+impl From<MlError> for LossError {
+    fn from(val: MlError) -> Self {
+        match val {
+            MlError::LossError(e) => e,
+            _ => unreachable!(),
+        }
+    }
+}
+
+impl From<String> for MlError {
+    fn from(error: String) -> Self {
+        MlError::StringError(error)
+    }
+}
+
+impl From<&str> for MlError {
+    fn from(error: &str) -> Self {
+        MlError::StringError(error.to_string())
+    }
+}
+
+pub type MlResult<T> = Result<T, MlError>;
