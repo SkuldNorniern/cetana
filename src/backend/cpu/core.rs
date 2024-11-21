01e471242f99dc7e81a97d3731e16a02f2a6fcf5
SkuldNorniern <skuldnorniern@gmail.com>
2024-11-22 00:21:14 +0900

    fix: reduce the size of flop calc

--- src/backend/cpu/core.rs
+++ src/backend/cpu/core.rs
@@ -20,7 +20,7 @@ impl CpuCore {
 
     pub fn calc_device_flops(&self) -> f64 {
         // Create two large tensors for benchmarking
-        let size = 1024;
+        let size = 256;
         let elements = size * size;
         
         let a = Tensor::from_vec(vec![1.0; elements], &[size, size]).unwrap();
@@ -64,9 +64,9 @@ mod tests {
     fn test_calc_device_flops() {
         let core = CpuCore::new();
         let flops = core.calc_device_flops();
-        println!("{}", pretty_flops(flops));
+        // println!("{}", pretty_flops(flops));
 
-        let flops = 900000.0;
+        // let flops = 900000.0;
         if flops < 100_000_00.0 {
             panic!(
                 "How are you even running this test?\nAre you using a Potato?\nFLOPS: {}",
@@ -76,6 +76,12 @@ mod tests {
         if flops > 1_000_000_000_000.0 {
             panic!(
                 "WTF? Are you using a supercomputer?\nFLOPS: {}",
+                pretty_flops(flops)
+            );
+        }
+        if 1_000_000_000.0 < flops.clone() && flops < 500_000_000_000.0 {
+            panic!(
+                "You're using a average computer\nFLOPS: {}",
                 pretty_flops(flops)
             );
         }
